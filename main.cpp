eca9d3002c708bdcf2626bd4267bf858d06ae4de
dopebeats55 <robjrbeats@gmail.com>
2020-01-19 23:18:09 +0000

    nouns and verbs assignment

--- main.cpp
+++ main.cpp
@@ -33,89 +33,109 @@ arm.rotateCounterClockwise(); //demonstrates CamelCase
 
 /*
  1)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun:pimp
+ action 1: the pimp finds hoes
+ action 2: the pimp sells hoes
+ action 3: the pimp disciplines hoes
  */
-
+pimp.findHoes();
+pimp.sellHoes();
+pimp.disciplineHoes();
 
 /*
  2)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: football
+ action 1: throw football
+ action 2: run football
+ action 3: kick football
  */
-
+football.throw();
+football.run();
+football.kick();
 /*
  3)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: basketball
+ action 1: shoot basketball
+ action 2: pass basketball
+ action 3: dribble basketball
  */
-
+basketBall.shoot();
+basketBall.pass();
+basketBall.dribble();
 
 /*
  4)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: food
+ action 1: prepare food
+ action 2: bake food
+ action 3: fry food
  */
-
+food.prepare();
+food.bake():
+food.fry();
 /*
  5)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: song
+ action 1: write song
+ action 2: record song
+ action 3: perform song
  */
-
+song.write();
+song.record();
+song.perform();
 
 /*
  6)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: code
+ action 1: make pseudocode
+ action 2: write code
+ action 3: complie code
  */
-
+code.makePsuedo();
+code.writeCode():
+code.compline();
 /*
  7)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: sex
+ action 1: gethead
+ action 2: getpenetration
+ action 3: kickherout
  */
-
+sex.getHead();
+sex.getPenetration();
+sex.kickHerOut();
 
 /*
  8)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: cellphone
+ action 1: text 
+ action 2: call
+ action 3: download apps
  */
-
+cellPhone.text();
+cellPhone.call();
+cellPhone.downloadApps();
 /*
  9)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: study
+ action 1: write flashcards
+ action 2: read chapters
+ action 3: take pratice tests
  */
-
+study.writeFlashCards();
+study.readChapters();
+study.takePraticeExams();
 
 /*
  10)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: clothes
+ action 1: wash clothes
+ action 2: dry clothes
+ action 3: fold clothes
  */
-
+clothes.wash();
+clothes.dry();
+clothes.fold();
 
 #endif
 
